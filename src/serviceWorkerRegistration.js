9b84d95bce0c8632c91104def5f9a8cb37ab9039
Denis Wanjohi <kazibin66@gmail.com>
2024-11-09 14:11:53 +0300

    Add PWA

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -14,7 +14,8 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL || ''}/service-worker.js`;
+
   
         if (isLocalhost) {
           checkValidServiceWorker(swUrl, config);
