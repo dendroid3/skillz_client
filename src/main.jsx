67e2d7d831e139ad20903f460445265d37342c42
Denis Wanjohi <kazibin66@gmail.com>
2024-11-09 14:00:43 +0300

    Add PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-
-// import Home from './pages/Home.jsx';
-//import Profile from './pages/InstructorProfile.jsx';
-
-
+// Your regular app render logic
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-
   </React.StrictMode>
 );
 
-  
-
+// Register the service worker to enable PWA features
+serviceWorkerRegistration.register();
